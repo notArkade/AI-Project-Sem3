50ca33ac368844bcf110f2e2daf872c899b20eec
Arkadip <arkadip671@gmail.com>
2024-10-28 21:56:03 +0530

    work in progress

--- main_video.py
+++ main_video.py
@@ -17,10 +17,10 @@ while True:
         cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
 
-    cv2.imshow("Frame", frame)
+    cv2.imshow("FameFrame", frame)
 
     key = cv2.waitKey(1)
-    if key == 27:
+    if key == 27: 
         break
 
 cap.release()
